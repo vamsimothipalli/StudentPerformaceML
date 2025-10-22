5f0ccf9df24df66877826094b64857669c9f5c58
vamsimothipalli <mothipalliv@gmail.com>
2025-10-22 14:01:07 -0500

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -76,7 +76,7 @@ class DataTransformation:
       logging.info(
         f"Applying preprocessing object on training dataframe and testing dataframe."
       )
-      print(input_feature_train_df.columns.tolist())
+      # print(input_feature_train_df.columns.tolist())
       input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
       input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
       train_arr = np.c_[
